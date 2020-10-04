b52879d6b2507fe16701600d3d0e9211857828ad
martycabj1999 <martingira1999@gmail.com>
2020-10-03 23:57:18 -0300

    liste los post con graphql

---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { listPosts } from '../graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
+
+class DisplayPosts extends Component {
+
+    state = {
+        ownerId: "",
+        ownerUsername: "",
+        errorMessage: "",
+        postLikedBy: [],
+        isHovering: false,
+        posts: []
+    }
+
+    componentDidMount = async () => {
+        this.getPosts()
+    }
+
+    getPosts = async () => {
+        const result = await API.graphql(graphqlOperation(listPosts))
+        console.log(result);
+        this.setState({ posts: result.data.listPosts.items })
+    }
+
+
+    render() {
+        const { posts } = this.state
+
+        return posts.map((post) => {
+
+            return (
+                <div className="posts" style={rowStyle} key={post.id}>
+                    <h1> {post.postTitle}</h1>
+                    <span style={{ fontStyle: "italic", color: "#0ca5e297" }}>
+                        {"Wrote by: "} {post.postOwnerUsername}
+
+                        {" on "}
+                        <time style={{ fontStyle: "italic" }}>
+                            {" "}
+                            {new Date(post.createdAt).toDateString()}
+
+                        </time>
+
+                    </span>
+
+                    <p> {post.postBody}</p>
+
+                    <br />
+
+                </div>
+            )
+
+        })
+    }
+}
+
+const rowStyle = {
+    background: '#f4f4f4',
+    padding: '10px',
+    border: '1px #ccc dotted',
+    margin: '14px'
+}
+export default DisplayPosts;
