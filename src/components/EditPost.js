ed7ab3a40bd8fc7429999edac20d382270a9b800
Ramiro Cerdá <rami992009@gmail.com>
2020-10-15 19:41:20 -0300

    agregue el modal para editar el tramite pero todavia le faltan los handlers

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,8 +1,74 @@
+import { Auth } from "aws-amplify";
 import React, { Component } from "react";
 
 class EditPost extends Component {
+  state = {
+    show: false,
+    id: "",
+    postOwnerId: "",
+    postOwnerUsername: "",
+    postTitle: "",
+    postBody: "",
+    postData: {
+      postTitle: this.props.postTitle,
+      postBody: this.props.postBody,
+    },
+  };
+
+  handleModal = () => {
+    this.setState({ show: !this.state.show });
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
+  componentWillMount = async () => {
+    await Auth.currentUserInfo().then((user) => {
+      this.setState({
+        postOwnerId: user.attributes.sub,
+        postOwnerUsername: user.username,
+      });
+    });
+  };
+
   render() {
-    return <button style={{ border: "none", marginRight: "1px" }}>Edit</button>;
+    return (
+      <>
+        {this.state.show && (
+          <div className="modal">
+            <button className="close" onClick={this.handleModal}>
+              X
+            </button>
+            <form
+              className="add-post"
+              onSubmit={(event) => this.handleUpdatePost(event)}
+            >
+              <input
+                style={{ fontSize: "19px" }}
+                placeholder="Title"
+                type="text"
+                name="postTitle"
+                value={this.state.postData.postTitle}
+                onChange={this.handleTitle}
+              />
+              <input
+                style={{ height: "150px", fontSize: "19px" }}
+                type="text"
+                name="postBody"
+                value={this.state.postData.postBody}
+                onChange={this.handleBody}
+              />
+              <button>Update Post</button>
+            </form>
+          </div>
+        )}
+        <button
+          onClick={this.handleModal}
+          style={{ border: "none", marginRight: "1px" }}
+        >
+          Edit
+        </button>
+      </>
+    );
   }
 }
 export default EditPost;
