6cded0bc39a5d47607cb3f13120d68e9c468250b
fran_gm <gabixp@hotmail.com.ar>
2020-11-11 01:49:06 -0300

    Avances sobre la subida y los get de imagenes. Faltaría crear una tabla de imagenes con post id al momento de subir la imagen, para tener una relacion muchos a uno contra los posts

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import DisplayPosts from './components/DisplayPosts';
 import { withAuthenticator } from 'aws-amplify-react';
 import '@aws-amplify/ui/dist/style.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <CreatePost />
       <DisplayPosts />
+
     </div>
   );
 }
--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,7 +1,35 @@
 import { API, Auth, graphqlOperation } from "aws-amplify";
 import React, { Component } from "react";
 import { updatePost } from "../graphql/mutations";
+import {Storage} from 'aws-amplify'
+
+  console.log('cargando imagen0')
+  let imageUrl =Storage.get('public/550x733_0BA.jpg')
+
 class EditPost extends Component {
+ // stateImg= {fileUrl:'',file:'', filename:''}
+  handleChange= e=>{
+    const file =e.target.files[0];
+    this.setState(({
+      storageImg:{
+          fileUrl: URL.createObjectURL(file),
+          file,
+          filename: file.name
+        }
+    }))
+  }
+  saveFile=()=>{
+    Storage.put(this.state.storageImg.filename,this.state.storageImg.file)
+      .then(()=>{
+        console.log('La imagen ha sido guardada exitosamente')
+        this.setState({storageImg:{fileUrl:'',file:'',filename:''}})
+      }).catch(err=>{
+        console.log('Hubo un error al guardar la imagen',err)
+      })
+  }
+
+
+
   state = {
     show: false,
     id: "",
@@ -13,6 +41,7 @@ class EditPost extends Component {
       postTitle: this.props.postTitle,
       postBody: this.props.postBody,
     },
+    storageImg:{fileUrl:'',file:'', filename:''}
   };
 
   handleModal = () => {
@@ -50,8 +79,29 @@ class EditPost extends Component {
       },
     });
   };
-
+   /*handleImage(){
+    console.log('cargando imagen0')
+    let imageUrl =Storage.get('public/550x733_0BA.jpg')
+    this.setState({
+      storageImg:{fileUrl:imageUrl}
+    })
+    console.log('cargando imagen',imageUrl)
+  }
+  asd(){
+    console.log('cargando imagen0')
+  }*/
+  componentDidMount = async () =>{
+    Storage.get('Victoria.png')
+    .then(data => {
+      this.setState({
+        storageImg:{fileUrl:data}
+      })
+      
+    console.log('hola',data)
+    })
+  }
   componentWillMount = async () => {
+   
     await Auth.currentUserInfo().then((user) => {
       this.setState({
         postOwnerId: user.attributes.sub,
@@ -87,8 +137,15 @@ class EditPost extends Component {
                 value={this.state.postData.postBody}
                 onChange={this.handleBody}
               />
+               <input type="file" onChange={this.handleChange} accept="image/x-png,image/gif,image/jpeg,image/png"/>
+              <img src={this.state.storageImg.fileUrl} alt="postImage" onChange={this.handleImage} style={{height:'200px',width:'400px'}}/>
+
+              <button onClick={this.saveFile}>Guardar imagen</button>
+
               <button>Editar Post</button>
+             
             </form>
+        
           </div>
         )}
         <button
